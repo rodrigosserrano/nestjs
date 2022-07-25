9560d2056c4de4ff26790df8f041c82e8a8c97d2
rodrigo.serrano <rodrigo.serrano@bomvalor.com.br>
2022-07-25 15:20:55 -0300

    feat: add endpoint create

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { MessagesService } from './messages.service';
+import { Message } from './Message';
 
 @Controller('messages')
 export class MessagesController {
@@ -8,5 +9,18 @@ export class MessagesController {
   @Get()
   findAll() {
     return this.messagesService.findAll();
+  }
+
+  @Get(':id')
+  findById(@Param() params) {
+    //Está recebendo uma string
+    // console.log(params.id, typeof params.id);
+    //colocando um '+' entende que é um number
+    return this.messagesService.findById(+params.id);
+  }
+
+  @Post()
+  create(@Body() body: Message) {
+    return this.messagesService.create(body);
   }
 }
--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,19 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { Message } from './Message';
 
 @Injectable()
 export class MessagesService {
-  public messages = [
+  public messages: Message[] = [
     {
       id: 1,
       text: 'Primeira mensagem',
     },
     {
-      id: 1,
-      text: 'Primeira mensagem',
+      id: 2,
+      text: 'Segunda mensagem',
     },
   ];
 
   findAll() {
     return this.messages;
+  }
+
+  findById(id: number) {
+    //está validando se o ID é o mesmo ID que existe no array, porém valida também o tipo
+    return this.messages.find((message) => message.id === id);
+  }
+
+  create(body: Message) {
+    return this.messages.push(body);
   }
 }
